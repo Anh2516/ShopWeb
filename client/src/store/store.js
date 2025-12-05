dc0e3859874d3ba475f98b89d31f4a900dccfd9b
P-code2025 <phu.b24cc225@stu.ptit.edu.vn>
2025-12-05 23:57:30 +0700

    fix: ko tải đc

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,14 +1,16 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/authSlice';
-import productReducer from './slices/productSlice';
-// import cartReducer from './slices/cartSlice'; // Vẫn giữ comment
-// import orderReducer from './slices/orderSlice'; // Vẫn giữ comment
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./slices/authSlice";
+import productReducer from "./slices/productSlice";
+import cartReducer from "./slices/cartSlice";
+import orderReducer from "./slices/orderSlice";
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     auth: authReducer,
-    products: productReducer, // <--- SỬA DÒNG NÀY: đổi 'product' thành 'products'
-    // cart: cartReducer,
-    // order: orderReducer,
+    products: productReducer,
+    cart: cartReducer,
+    orders: orderReducer,
   },
 });
+
+export default store;
