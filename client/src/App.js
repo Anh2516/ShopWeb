bce067692a7f3f32759684e4a167d7f5a29a8a86
TuHuynh <HuynhDangtu169@gmail.com>
2025-12-05 12:37:39 +0700

    Add Auth pages and API integration

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        {/* Navbar chưa có nên tạm thời comment lại */}
+        {/* <Navbar /> */}
+        
+        <Routes>
+          {/* Tạm thời redirect trang chủ về Login để test */}
+          <Route path="/" element={<Navigate to="/login" />} />
+          
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          
+          {/* Các route khác chưa làm xong thì chưa khai báo */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
